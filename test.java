4c848185a222f9881f630276ada2f87956067ead
Shan Bai <shan.bai@teradata.com>
2017-06-01 15:16:15 -0400

    third

--- test.java
+++ test.java
@@ -1,5 +1,11 @@
+package default;
+
 public class test{
     public static void main(String[] args){
 	System.out.println("Test starts:");
+
+        System.out.println("THIS IS BABY TRAVIS");
+
+	System.out.println("Thanks for working!");
     }
 }
